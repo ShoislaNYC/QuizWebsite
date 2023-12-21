c8c136d13680e85cdfc7c35a8496d2679f1bf070
Shohag <shoislamNYC@gmail.com>
2023-12-20 19:51:51 -0500

    Play button now leads to second page

--- client/src/components/PlayButton.jsx
+++ client/src/components/PlayButton.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function PlayButton(){
     return(
+        <Link to ="/Menu">
         <button onClick={()=> 
         console.log("clicked")}>
         PLAY</button>
+        </Link>
     )
 }
 
